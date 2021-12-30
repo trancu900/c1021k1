c25e6976cb66d839fef992b5987c797f6e1895f8
trancu900 <pet.cu20791@gmail.com>
2021-12-30 10:00:40 +0700

    fix

--- src/file/BinaryDemo.java
+++ src/file/BinaryDemo.java
@@ -1,17 +1,36 @@
 package file;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class BinaryDemo {
     public static void main(String[] args) {
+        writeFile();
+    }
+    public static void writeFile() {
+        try {
+            File file = new File("data\\test");
+            if (!file.exists())
+                file.createNewFile();
+
+            FileOutputStream outputStream = new FileOutputStream(file,true);
+            String goodbye = "Good bye Dan, Chuc Dan Binh An\nawfasef";
+            outputStream.write(goodbye.getBytes());
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void readFile() {
         try {
             long startMillis = System.currentTimeMillis();
             FileInputStream fis = new FileInputStream("C:\\Users\\Administrator\\Downloads\\front-dashboard.rar");
             int result;
             while ((result = fis.read()) > -1) {
             }
+            fis.close();
             System.out.println(System.currentTimeMillis() - startMillis);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
